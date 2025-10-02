028a221eed58eb2eb091c1898cd61442a9820061
Onyeka Nwokike <nwokikeonyeka@gmail.com>
2025-10-02 15:01:37 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,4 +8,4 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 
 # One-time superuser creation (run once, then remove these lines)
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='Maikel').exists() or User.objects.create_superuser('admin', 'nwokikeonyeka@gmail.com', 'Maikel1@')" | python manage.py shell
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='Maikel').exists() or User.objects.create_superuser('Maikel', 'nwokikeonyeka@gmail.com', 'Maikel1@')" | python manage.py shell
